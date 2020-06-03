d59f3b8f461fbcceda520c07e3f406f06b48a6b2
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-03 15:03:38 +0200

    Dashboard modal added

--- src/component/dashboardmap/Dashboard.js
+++ src/component/dashboardmap/Dashboard.js
@@ -1,65 +1,84 @@
-import React, {useState} from 'react';
-import NavDrawer from "../navigation/NavDrawer";
+import React, { useState, Fragment } from 'react';
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+import SendIcon from '@material-ui/icons/Send';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import HomeSharpIcon from '@material-ui/icons/HomeSharp';
-import ReplySharpIcon from '@material-ui/icons/ReplySharp';
-import PriorityHighSharpIcon from '@material-ui/icons/PriorityHighSharp';
-import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
-import ExitToAppSharpIcon from '@material-ui/icons/ExitToAppSharp';
-import logo from "../../img/Icon.png";
+import withStyles from '@material-ui/core/styles/withStyles';
 import plus from "../../img/plus.png";
+import NavDrawer from "../navigation/NavDrawer";
+import Modal from "../modals/Modals"
 
-
-{/*stijl element */}
-
-const useStyles = makeStyles({
+const styles = (theme) => ({
+  ...theme.spreadThis,
   list: {
     background: '#2980B9 -webkit-linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9) linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9)',
     borderRadius: 2,
-    width: 175, 
-    height:1900,
+    width: 175,
+    height: 1900,
     padding: 100,
-   
+
   },
   fullList: {
     width: 'auto',
   },
   container: {
-      position: 'relative',
-      color: '#94F0FF',
-      fontSize: 24
+    position: 'relative',
+    color: '#94F0FF',
+    fontSize: 24
   },
   plusicon: {
-      position: 'absolute',
-      right: 10
+    position: 'absolute',
+    right: 10
   },
-    extra: {
-        margin: 100
-    }
+  extra: {
+    margin: 100
+  },
+  border: {
+    borderColor: '#94F0FF',
+    color: '#94F0FF'
+  }
 });
 
-export default function Dashboard() {
-  const classes = useStyles();
+function Dashboard(props) {
+  const {classes} = props
+
+  const modalDetails = () => {
+    return (
+        <Fragment>
+          <TextField variant="outlined" className={clsx(classes.inputs, classes.textColors)} label={"Title"} fullWidth/>
+          <TextField variant="outlined" className={clsx(classes.inputs, classes.textColors)} label={"Subject"} fullWidth/>
+          <TextField variant="outlined" className={clsx(classes.inputs, classes.textColors)} label={"Body"} fullWidth multiline={true}/>
+          <Button
+            className={clsx(classes.button, classes.textColors)}
+            variant="contained"
+            endIcon={<SendIcon>send</SendIcon>}
+          >
+            Send
+          </Button>
+        </Fragment>
+    )
+  }
 
   return (
-      <NavDrawer>
-          <h2 className="overview">Overview</h2>
-          <div className={clsx(classes.container)}>
-              <p className={clsx(classes.inbox, classes.extra)}>Inbox: 250 emails </p>
-              <p className={clsx(classes.trashtext, classes.extra)}>Trash: 25 mails </p>
-              <p className={clsx(classes.readtext, classes.extra)}>Read: 5 emails </p>
-              <p className={clsx(classes.unreaden, classes.extra)}>Unread: 245 emails </p>
-              <p className={clsx(classes.labeltext, classes.extra)}>Labels: 5 </p>
-              <a href= "/dashboard"><img className={clsx(classes.plusicon)} src={plus} /></a>  {/* plus icoon van dashboard */}
-          </div>
-      </NavDrawer>
+    <NavDrawer>
+      <h2 className="overview"> Overview </h2>
+      <div className={clsx(classes.container)}>
+        <p className={clsx(classes.extra)}> Inbox: 250 emails </p>
+        <p className={clsx(classes.extra)}> Trash: 25 mails </p>
+        <p className={clsx(classes.extra)}> Read: 5 emails </p>
+        <p className={clsx(classes.extra)}> Unread: 245 emails </p>
+        <p className={clsx(classes.extra)}> Labels: 5 </p>
+        <div className={clsx(classes.plusicon)}>
+          <Modal 
+            title="New Email"
+            rest={modalDetails}
+          >
+            <img className={clsx(classes.plusicon)} src={plus} />
+          </Modal>
+        </div>
+      </div>
+    </NavDrawer>
   )
 }
+
+export default (withStyles(styles)(Dashboard))
