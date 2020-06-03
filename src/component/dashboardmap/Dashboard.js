e1ea51e6a13a5d12c17c48c675b6c51ce93d4d18
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-03 15:04:55 +0200

    useState removed

--- src/component/dashboardmap/Dashboard.js
+++ src/component/dashboardmap/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 import SendIcon from '@material-ui/icons/Send';
