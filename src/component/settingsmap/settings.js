de80d826f0bbf5a90b986866bcadf54137fc93b0
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-04 14:57:59 +0200

    Navdrawer added

--- src/component/settingsmap/settings.js
+++ src/component/settingsmap/settings.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import HomeSharpIcon from '@material-ui/icons/HomeSharp';
-import ReplySharpIcon from '@material-ui/icons/ReplySharp';
-import PriorityHighSharpIcon from '@material-ui/icons/PriorityHighSharp';
-import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
-import ExitToAppSharpIcon from '@material-ui/icons/ExitToAppSharp';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -21,6 +9,7 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import NavDrawer from "../navigation/NavDrawer"
 import logo from "../../img/Icon.png";
 
 function Alert(props) {
@@ -80,90 +69,9 @@ function CustomizedSnackbars() {
     );
 }
 
-export default function SwipeableTemporaryDrawer() {
-    const classes = useStyles();
-    const [state, setState] = React.useState({
-        left: false
-    });
+export default function Settings() {
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-
-        setState({ ...state, [anchor]: open });
-    };
-
-    const list = (anchor) => (
-        <div
-            className={clsx(classes.list, {
-                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            {/* Dashboard Navigatie */}
-            <a href='/dashboard'>
-                <List>
-                    {['Home'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon> <HomeSharpIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </a>
-            <Divider />
-            <Divider />
-            <a href='/autoreplies'>
-                <List>
-                    {['Auto Replies'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon> <ReplySharpIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </a>
-            <Divider />
-            <a href='/spammedUsers'>
-                <List>
-                    {['Spammed Users'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon> <PriorityHighSharpIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </a>
-            <Divider />
-            <a href='/settings'>
-                <List>
-                    {['Settings'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon> <SettingsSharpIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </a>
-            <Divider />
-            <a href='/signout'>
-                <List>
-                    {['Signout'].map((text) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon> <ExitToAppSharpIcon /></ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </a>
-            <Divider />
-        </div>
-    );
-
-    const [checked, setChecked] = React.useState(true);
+    const [checked, setChecked] = React.useState(false);
 
     const handleChangeCheckbox = (event) => {
         setChecked(event.target.checked);
@@ -176,23 +84,8 @@ export default function SwipeableTemporaryDrawer() {
     };
 
     return (
-        <div>
-            {['OPEN MENU'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                    <Button className= "knopje" onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                    <SwipeableDrawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
-                        onOpen={toggleDrawer(anchor, true)}
-                    >
-                        {list(anchor)}
-                    </SwipeableDrawer>
-                </React.Fragment>
-            ))}
+        <NavDrawer>
             <h2 className="overview">My data</h2>
-            {/* logo image */}
-            <a href= "/dashboard"><img className= "db-logo" src={logo} width="70" height="50" alt="logo" /></a>
             <div className="settings-container">
                 <input className="settings-email" placeholder="Email address"/>
                 <input className="settings-username" placeholder="Username"/>
@@ -202,27 +95,39 @@ export default function SwipeableTemporaryDrawer() {
                         <FormGroup aria-label="position" row>
                             <FormControlLabel
                                 value="end"
-                                control={<Checkbox style={{ color: '#94F0FF', top: '11px', left: '30px' }}
-                                                   checked={checked}
-                                                   onChange={handleChangeCheckbox}
-                                />}
+                                control={
+                                    <Checkbox 
+                                        style={{ color: '#94F0FF', top: '11px', left: '30px' }}          
+                                        checked={checked}           
+                                        onChange={handleChangeCheckbox}
+                                    />
+                                }
                             />
                         </FormGroup>
                     </FormControl>
                     <span className="settings-box-settings">Enable two step authentication:</span>
                     <FormControl component="fieldset" className="radios">
                         <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                            <FormControlLabel value="phone" control={<Radio style={{ color: '#94F0FF' }}/>}
-                                              label="Phone number"
-                                              style={{ color: 'white' }}
+                            <FormControlLabel 
+                                value="phone" 
+                                control={
+                                    <Radio 
+                                        style={{ color: '#94F0FF' }}
+                                    />
+                                }
+                                label="Phone number"
+                                style={{ color: 'white' }}
+                                disabled={checked ? false : true}
                             />
                             <FormControlLabel value="email" control={<Radio style={{ color: '#94F0FF' }}/>}
                                               label="Email address"
                                               style={{ color: 'white' }}
+                                              disabled={checked ? false : true}
                             />
                             <FormControlLabel value="qr" control={<Radio style={{ color: '#94F0FF' }}/>}
                                               label="QR-code"
                                               style={{ color: 'white' }}
+                                              disabled={checked ? false : true}
                             />
                         </RadioGroup>
                     </FormControl>
@@ -232,6 +137,6 @@ export default function SwipeableTemporaryDrawer() {
                     <span> </span>
                 </footer>
             </div>
-        </div>
+        </NavDrawer>
     );
 }
