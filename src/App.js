add4c5d92ddc3074fd292f52fc6fb15294e90dcc
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-04 14:29:54 +0200

    component renamed

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import verifyEmail from "./component/signupmap/verifyEmail";
 import verifyLogin from "./component/loginmap/verifyLogin";
 import Dashboard from './component/dashboardmap/Dashboard';
 import AutoReplies from './component/automap/AutoReplies';
-import spammedUsers from './component/spammap/spammedUsers';
+import SpammedUsers from './component/spammap/spammedUsers';
 import Settings from './component/settingsmap/settings';
 import Signout from './component/signoutmap/Signout';
 
@@ -56,7 +56,7 @@ export default function App() {
             <Route path='/dashboard' component={Dashboard} />
             <Route path='/settings' component={Settings} />
             <Route path='/autoreplies' component={AutoReplies} />
-            <Route path='/spammedUsers' component={spammedUsers} />
+            <Route path='/spammedUsers' component={SpammedUsers} />
             <Route path='/signout' component={Signout} />
           </Switch>
         </div>
