4bf071130afb4e3cb77e38cb205d5632273ec0d1
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-18 12:57:06 +0200

    stop loading ui type added

--- src/redux/reducers/uiReducer.js
+++ src/redux/reducers/uiReducer.js
@@ -1,4 +1,4 @@
-import { SET_ERRORS, LOADING_UI, CLEAR_ERRORS } from '../types'
+import { SET_ERRORS, LOADING_UI, CLEAR_ERRORS, STOP_LOADING_UI } from '../types'
 
 const initialState = {
     loading: false,
@@ -23,6 +23,12 @@ export default function (state = initialState, action) {
                 ...state,
                 loading: false,
                 errors: null
+            }
+
+        case STOP_LOADING_UI:
+            return {
+                ...state,
+                loading: false
             }
         default:
             return state
