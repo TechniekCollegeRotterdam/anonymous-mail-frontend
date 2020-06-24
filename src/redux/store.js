26d67ed71ec8757462fdaa68a329842d932c1176
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-06-24 12:58:36 +0200

    action logger removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,3 @@
-/* import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk';
-
-import userReducer from './reducers/userReducer'
-import dataReducer from './reducers/dataReducer'
-import uiReducer from './reducers/uiReducer'
-
-const initialState = {}
-
-const middleware = [thunk]
-
-const reducers = combineReducers({
-    user: userReducer,
-    data: dataReducer,
-    ui: uiReducer
-})
-
-const store = createStore(
-    reducers,
-    initialState,
-    compose(
-        applyMiddleware(...middleware)
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-export default store */
-
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import userReducer from './reducers/userReducer'
 import dataReducer from './reducers/dataReducer'
@@ -34,11 +6,11 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
 
-const logger = createLogger({
-    /* https://github.com/evgenyrodionov/redux-logger */
+/*const logger = createLogger({
+    /!* https://github.com/evgenyrodionov/redux-logger *!/
     collapsed: true,
     diff: true
-});
+});*/
 
 const initialState = {}
 
@@ -54,7 +26,8 @@ const store = createStore(
     reducers,
     initialState,
     composeWithDevTools(
-        applyMiddleware(...middleware, logger)
+        //applyMiddleware(...middleware, logger)
+        applyMiddleware(...middleware)
     )
 )
 
